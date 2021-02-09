053e0a75d2bcd5a9ab4162ca5f0d6c54553f4fda
julio <juliocnl92@gmail.com>
2021-02-09 08:15:37 -0700

    primero

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+};
+function click_boton(){
+    //let lista = document.getElementById('lista');
+    //lista.innerHTML = "<div class='amarillo'>s sdfsdfsdfsdfs </div>";
+    cargar_datos()
+}
+
+
+function hola_mundo(){
+    alert("hola mundo");
+}
+
+
+function cargar_datos(){
+    fetch('https://localhost:44347/api/values')
+        .then(response => response.json())
+        .then(data =>{
+            let lista = document.getElementById('lista');
+            data.forEach(element => {
+                lista.innerHTML += '<div>'+element+'</div>';
+            });
+        });
+  }
